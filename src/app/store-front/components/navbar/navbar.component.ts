fedd01de7df6ff6d90b161a66002bdee42017a3f
miguel angel <miguel33martin@outlook.es>
2025-10-11 11:18:00 +0200

    desarrollo: generada estructura básica del proyecto

---
+++ src/app/store-front/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'front-navbar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+
+}
