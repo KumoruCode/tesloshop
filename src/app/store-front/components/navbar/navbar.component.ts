2731483af478b90467efa7f8d5cb9bff516eeeb2
miguel angel <miguel33martin@outlook.es>
2025-12-31 16:03:50 +0100

    mostar usuario y cierre de sesión

--- src/app/store-front/components/navbar/navbar.component.ts
+++ src/app/store-front/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'front-navbar',
@@ -7,5 +8,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+
+  authService = inject(AuthService);
 
 }
