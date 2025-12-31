98a2cac77892ea736ba6acc9df75b4e03ae577a1
miguel angel <miguel33martin@outlook.es>
2025-12-31 10:26:20 +0100

    implementando servicio de autenticación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+    // TODO: Guards
+  },
+
 
   {
     path: '',
