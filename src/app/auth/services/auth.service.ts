4e001bc0f77ac2a802be103da13d9327e2e47be9
miguel angel <miguel33martin@outlook.es>
2025-12-31 10:41:20 +0100

    desarrollando login en authService

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
+import { AuthResponse } from '@auth/interfaces/auth-response.interface';
 import { User } from '@auth/interfaces/user.interface';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-type AuthStatus = 'checking' | 'authentiucated' | 'not-authenticated';
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
 const baseUrl = environment.baseUrl;
 
 @Injectable({
@@ -28,10 +30,17 @@ export class AuthService {
   user = computed<User|null>(() => this._user());
   token = computed(this._token);
 
-  login(email: string, password: string){
-    return this.http.post(`${baseUrl}/auth/login`, {
+  login(email: string, password: string): Observable<AuthResponse>{
+    return this.http.post<AuthResponse>(`${baseUrl}/auth/login`, {
       email: email,
       password: password,
-    })
+    }).pipe(
+      tap( resp => {
+        this._user.set(resp.user);
+        this._authStatus.set('authenticated');
+        this._token.set(resp.token);
+        localStorage.setItem('token', resp.token);
+      })
+    )
   }
 }
